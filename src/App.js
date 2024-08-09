24163cf5e8f5cd24d62771b394ca8429430d445e
AbdurRahman2004 <ar978874@gmail.com>
2024-08-09 07:01:48 +0530

    Initail Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {  Routes, Route } from "react-router-dom";
+import About from './component/about';
+import Home from './component/home';
+import Post from './component/post';
+import CreatePost from './component/createpost';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './component/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+       <NavBar />
+
+      <Routes>
+        <Route path='home' element={<Home />} ></Route>
+        <Route path='post' element={<Post />} ></Route>
+        <Route path='about' element={<About />} ></Route>
+
+        <Route path='createpost' element={<CreatePost />} ></Route>
+      </Routes>
+     
+
+      </div>
+
+      )}
 
 export default App;
---
+++ src/component/createpost.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreatePost(){
+     return (
+        <>
+        Create Post!
+        </>
+     )
+}
+
+export default CreatePost;
